1ce8e79f6fb9aac4652d644b02f273cf8b291234
Gabriel Oliveira <gabriel.dev04@gmail.com>
2022-08-07 12:00:09 -0300

    Alterado a criação do objeto, adicionado função de validação de campos e formatação de campos

--- app.js/logica.js
+++ app.js/logica.js
@@ -1,24 +1,59 @@
 
-
-function receberValor() {
-    const purchase = {
-        data: document.getElementById("inptData").value,
-        name: document.getElementById("inptName").value,
-        price: document.getElementById("inptPrice").value
-    }
-    var tr = document.createElement("tr");
-    document.getElementById("table").appendChild(tr);
-
-    for (var i in purchase) {
-        var td = document.createElement("td");
-        tr.appendChild(td).innerHTML = compra[i];
-    }
-    clearField();
+function Purchase(data, product, price) {
+    this.data = new Date(data);
+    this.product = product;
+    this.price = Number(price);
 }
+
+function reciveFields() {
+    let data = document.getElementById("inptData").value
+    let product = document.getElementById("inptProduct").value
+    let price = document.getElementById("inptPrice").value
+
+    const p = new Purchase(data, product, price);
+    if (data == "" || product == "" || price == "") {
+        alert(`Um ou mais campos não foram 
+        preenchidos`);
+    } else {
+        registerField(p);
+        clearField();
+    }
+}
+
+function registerField(p) {
+    let tr = document.createElement("tr");
+    document.getElementById("table").appendChild(tr);
+    for (var i in p) {
+        var td = document.createElement("td");
+        tr.appendChild(td).innerHTML = formatField(p[i]);
+    }
+}
+
 function clearField() {
     document.getElementById("inptData").value = "";
-    document.getElementById("inptName").value = "";
-    document.getElementById("inptPreco").value = "";
+    document.getElementById("inptProduct").value = "";
+    document.getElementById("inptPrice").value = "";
 }
 
+function formatField(field) {
+    if (typeof field === 'object') {
+        const monthTheYears = [
+            "Janeiro", "Fevereiro", "Março",
+            "Abril", "Maio", "Junho", "Julho", "Agosto",
+            "Setembro", "Outubro", "Novembro", "Dezembro"]
+        let text = ''
+        let day = field.getDay();
+        let month = field.getMonth();
+        for(i=0;i<monthTheYears.length; i++){
+            if(month == i){
+                month = monthTheYears[i];
+            }
+        }
+        return text = `Dia: ${day} / ${month}`
+    }if(typeof field === 'string'){
+        return text = `Compra: ${field}`
 
+    }else{
+        return text = `R$: ${field.toFixed(2)}`;
+    }
+}
